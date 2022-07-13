245f9dfcd1542fe618e6c1a7ec5bfd4e2f3176da
konkarin <konkarin.photo@gmail.com>
2022-07-13 16:30:45 +0900

    init

---
+++ components/GoTemplate.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{ label }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GoTemplate",
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.nuxt-logo {
+  height: 180px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <GoTemplate :label="label" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "IndexPage",
+  asyncData() {
+    return {
+      label: "{{ variant }}",
+    };
+  },
+});
+</script>
